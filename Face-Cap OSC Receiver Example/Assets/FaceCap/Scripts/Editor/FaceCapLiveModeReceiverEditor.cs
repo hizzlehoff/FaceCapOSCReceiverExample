72306bf6b3fb190d790e05d2ac17af53a94ba3aa
niels jansson <hizzlehoff@users.noreply.github.com>
2020-06-20 09:41:00 +0200

    Complete rewrite of the face cap receiver component.

---
+++ Face-Cap OSC Receiver Example/Assets/FaceCap/Scripts/Editor/FaceCapLiveModeReceiverEditor.cs
@@ -0,0 +1,156 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(FaceCapLiveModeReceiver))]
+public class FaceCapLiveModeReceiverEditor : Editor
+{
+    FaceCapLiveModeReceiver script;
+
+    string[] faceCapBlendshapeNames = { "browInnerUp", "browDown_L", "browDown_R", "browOuterUp_L","browOuterUp_R","eyeLookUp_L","eyeLookUp_R","eyeLookDown_L","eyeLookDown_R","eyeLookIn_L","eyeLookIn_R","eyeLookOut_L","eyeLookOut_R","eyeBlink_L","eyeBlink_R","eyeSquint_L","eyeSquint_R","eyeWide_L","eyeWide_R","cheekPuff","cheekSquint_L","cheekSquint_R","noseSneer_L","noseSneer_R","jawOpen","jawForward","jawLeft","jawRight","mouthFunnel","mouthPucker","mouthLeft","mouthRight","mouthRollUpper","mouthRollLower","mouthShrugUpper","mouthShrugLower","mouthClose","mouthSmile_L","mouthSmile_R","mouthFrown_L","mouthFrown_R","mouthDimple_L","mouthDimple_R","mouthUpperUp_L","mouthUpperUp_R","mouthLowerDown_L","mouthLowerDown_R","mouthPress_L","mouthPress_R","mouthStretch_L","mouthStretch_R","tongueOut","none"};
+    string[] polywinkBlendshapeNames = { "browInnerUp", "browDownLeft", "browDownRight", "browOuterUpLeft", "browOuterUpRight", "eyeLookUpLeft", "eyeLookUpRight", "eyeLookDownLeft", "eyeLookDownRight", "eyeLookInLeft", "eyeLookInRight", "eyeLookOutLeft", "eyeLookOutRight", "eyeBlinkLeft", "eyeBlinkRight", "eyeSquintLeft", "eyeSquintRight", "eyeWideLeft", "eyeWideRight", "cheekPuff", "cheekSquintLeft", "cheekSquintRight", "noseSneerLeft", "noseSneerRight", "jawOpen", "jawForward", "jawLeft", "jawRight", "mouthFunnel", "mouthPucker", "mouthLeft", "mouthRight", "mouthRollUpper", "mouthRollLower", "mouthShrugUpper", "mouthShrugLower", "mouthClose", "mouthSmileLeft", "mouthSmileRight", "mouthFrownLeft", "mouthFrownRight", "mouthDimpleLeft", "mouthDimpleRight", "mouthUpperUpLeft", "mouthUpperUpRight", "mouthLowerDownLeft", "mouthLowerDownRight", "mouthPressLeft", "mouthPressRight", "mouthStretchLeft", "mouthStretchRight", "tongueOut", "none" };
+
+    void OnEnable()
+    {
+        script = (FaceCapLiveModeReceiver)target;
+    }
+
+    public override void OnInspectorGUI()
+    {
+        script._OSCReceiverPort = EditorGUILayout.IntField("OSC Port", script._OSCReceiverPort);
+
+        EditorGUILayout.LabelField("", GUI.skin.horizontalSlider);
+
+        script.blendshapeMesh = EditorGUILayout.ObjectField("Blendshape Mesh", script.blendshapeMesh, typeof(GameObject), true) as GameObject;
+
+        EditorGUILayout.LabelField("", GUI.skin.horizontalSlider);
+
+        script.usePositionData = EditorGUILayout.Toggle("Use Position Data", script.usePositionData);
+        script.useRotationData = EditorGUILayout.Toggle("Use Rotation Data", script.useRotationData);
+
+        if (script.usePositionData || script.useRotationData)
+        {
+            EditorGUILayout.Space();
+
+            script.headTransform = EditorGUILayout.ObjectField("Head Transform", script.headTransform, typeof(Transform), true) as Transform;
+
+            script.neckTransformEnabled = EditorGUILayout.Toggle("Neck", script.neckTransformEnabled);
+            if (script.neckTransformEnabled)
+            {
+                script.neckTransform = EditorGUILayout.ObjectField("Neck Transform", script.neckTransform, typeof(Transform), true) as Transform;
+                script.neckTransformBlendFactor = EditorGUILayout.FloatField("Neck Blend", script.neckTransformBlendFactor);
+            }
+
+            script.spineTransformEnabled = EditorGUILayout.Toggle("Spine", script.spineTransformEnabled);
+            if (script.spineTransformEnabled)
+            {
+                script.spineTransform = EditorGUILayout.ObjectField("Spine Transform", script.spineTransform, typeof(Transform), true) as Transform;
+                script.spineTransformBlendFactor = EditorGUILayout.FloatField("Spine Blend", script.spineTransformBlendFactor);
+            }
+        }
+
+        EditorGUILayout.LabelField("", GUI.skin.horizontalSlider);
+
+        script.useEyeDirectionData = EditorGUILayout.Toggle("Use Eye Direction Data", script.useEyeDirectionData);
+
+        
+        if (script.useEyeDirectionData)
+        {
+            EditorGUILayout.Space();
+
+            script.leftEyeTransform = EditorGUILayout.ObjectField("Left Eye Transform", script.leftEyeTransform, typeof(Transform), true) as Transform;
+            script.rightEyeTransform = EditorGUILayout.ObjectField("Right Eye Transform", script.rightEyeTransform, typeof(Transform), true) as Transform;
+        }
+
+        EditorGUILayout.LabelField("", GUI.skin.horizontalSlider);
+
+        EditorGUILayout.LabelField("Blendshape configuration:");
+
+        if (script.blendshapeMesh != null)
+        {
+            SkinnedMeshRenderer smr = script.blendshapeMesh.GetComponent<SkinnedMeshRenderer>();
+            if (!smr)
+            {
+                GUILayout.BeginHorizontal(EditorStyles.helpBox);
+                EditorGUILayout.LabelField("Warning: Assigned blendshape mesh has no skinned mesh renderer.");
+                GUILayout.EndHorizontal();
+                return;
+            }
+
+            if (smr.sharedMesh.blendShapeCount == 0)
+            {
+                GUILayout.BeginHorizontal(EditorStyles.helpBox);
+                EditorGUILayout.LabelField("Warning: Assigned blendshape mesh has no blendshapes.");
+                GUILayout.EndHorizontal();
+                return;
+            }
+
+            if (script.blendShapeIndexes == null)
+            {
+                InitializeBlendshapes(script, smr);
+                AutoConfigureBlendshapes(script, smr);
+            }
+
+            for (int i = 0; i < script.blendShapeIndexes.Length; i++)
+            {
+                GUILayout.BeginHorizontal(EditorStyles.label);
+                string blendShapeName = smr.sharedMesh.GetBlendShapeName(i);
+                script.blendShapeIndexes[i] = EditorGUILayout.Popup(blendShapeName, script.blendShapeIndexes[i], faceCapBlendshapeNames);
+                GUILayout.EndHorizontal();
+            }
+
+            GUILayout.BeginHorizontal(EditorStyles.helpBox);
+            if (GUILayout.Button("Initialize"))
+            {
+                InitializeBlendshapes(script, smr);
+            }
+            if (GUILayout.Button("Automatic"))
+            {
+                AutoConfigureBlendshapes(script, smr);
+            }
+            GUILayout.EndHorizontal();
+
+        }
+        else
+        {
+            GUILayout.BeginHorizontal(EditorStyles.helpBox);
+            EditorGUILayout.LabelField("Warning: Blendshape mesh not assigned.");
+            GUILayout.EndHorizontal();
+            return;
+        }
+
+        EditorGUILayout.Space();
+
+        EditorUtility.SetDirty(script);
+    }
+
+    void InitializeBlendshapes(FaceCapLiveModeReceiver script, SkinnedMeshRenderer smr)
+    {
+        script.blendShapeIndexes = new int[smr.sharedMesh.blendShapeCount];
+
+        for (int i = 0; i < script.blendShapeIndexes.Length; i++)
+        {
+            script.blendShapeIndexes[i] = faceCapBlendshapeNames.Length - 1;
+        }
+    }
+
+    void AutoConfigureBlendshapes(FaceCapLiveModeReceiver script, SkinnedMeshRenderer smr)
+    {
+        script.blendShapeIndexes = new int[smr.sharedMesh.blendShapeCount];
+
+        for (int i = 0; i < script.blendShapeIndexes.Length; i++)
+        {
+            string name = smr.sharedMesh.GetBlendShapeName(i);
+
+            for (int j=0; j < faceCapBlendshapeNames.Length; j++)
+            {
+                if (name.Contains(faceCapBlendshapeNames[j]) || name.Contains(polywinkBlendshapeNames[j]))
+                {
+                    script.blendShapeIndexes[i] = j;
+                }
+            }
+        }
+
+    }
+}
---
+++ Face-Cap OSC Receiver Example/Assets/FaceCap/Scripts/FaceCapLiveModeReceiver.cs
@@ -0,0 +1,278 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using extOSC;
+
+public class FaceCapLiveModeReceiver : MonoBehaviour
+{
+    [SerializeField]
+    public int _OSCReceiverPort = 9000;
+
+    private OSCReceiver _OSCReceiver;
+    private const string _Position = "/HT";
+    private const string _EulerAngles = "/HR";
+    private const string _LeftEyeEulerAngles = "/ELR";
+    private const string _RightEyeEulerAngles = "/ERR";
+    private const string _Blendshapes = "/W";
+
+    [SerializeField]
+    public GameObject blendshapeMesh;
+    [SerializeField]
+    public int[] blendShapeIndexes;
+    [SerializeField]
+    public SkinnedMeshRenderer smr;
+
+    [SerializeField]
+    public bool usePositionData = false;
+    Vector3 startPosition = new Vector3(0, 0, 0);
+
+    [SerializeField]
+    public bool useRotationData = false;
+
+    [SerializeField]
+    public Transform headTransform;
+    [SerializeField]
+    Quaternion headRotationOffset;
+
+    [SerializeField]
+    public bool neckTransformEnabled = false;
+    [SerializeField]
+    public Transform neckTransform;
+    [SerializeField]
+    public float neckTransformBlendFactor = 0.5f;
+    Quaternion neckRotationInitial;
+    Quaternion neckRotationOffset;
+
+    [SerializeField]
+    public bool spineTransformEnabled = false;
+    [SerializeField]
+    public Transform spineTransform;
+    [SerializeField]
+    public float spineTransformBlendFactor = 0.25f;
+    Quaternion spineRotationInitial;
+    Quaternion spineRotationOffset;
+
+    [SerializeField]
+    public bool useEyeDirectionData = false;
+
+    [SerializeField]
+    public Transform leftEyeTransform;
+    Quaternion leftEyeRotationOffset;
+
+    [SerializeField]
+    public Transform rightEyeTransform;
+    Quaternion rightEyeRotationOffset;
+
+    bool isEveryThingConfigured = true;
+
+    void Start()
+    {
+
+        if (blendshapeMesh == null)
+        {
+            isEveryThingConfigured = false;
+            Debug.LogWarning("Face Cap Live Mode Receiver Error : Blenshape mesh is not assigned.");
+        }
+        else
+        {
+            smr = blendshapeMesh.GetComponent<SkinnedMeshRenderer>();
+            if (smr == null)
+            {
+                isEveryThingConfigured = false;
+                Debug.LogWarning("Face Cap Live Mode Receiver Error : Blenshape mesh has no blendshapes.");
+            }
+            else
+            {
+                if (smr.sharedMesh.blendShapeCount == 0)
+                {
+                    isEveryThingConfigured = false;
+                    Debug.LogWarning("Face Cap Live Mode Receiver Error : Blenshape mesh has no blendshapes.");
+                }
+            }
+        }
+
+        if (usePositionData || useRotationData)
+        {
+            if (headTransform == null)
+            {
+                isEveryThingConfigured = false;
+                Debug.LogWarning("Face Cap Live Mode Receiver Error : Head transform is not assigned.");
+            }
+            else
+            {
+                headRotationOffset = Quaternion.Inverse(ConvertScneneKitSpaceToUnitySpace(new Vector3(0, 0, 0))) * headTransform.rotation;
+                startPosition = headTransform.localPosition;
+            }
+
+            if (neckTransformEnabled)
+            {
+                if (neckTransform == null)
+                {
+                    isEveryThingConfigured = false;
+                    Debug.LogWarning("Face Cap Live Mode Receiver Error : Neck transform is not assigned.");
+                }
+                else
+                {
+                    neckRotationInitial = neckTransform.rotation;
+                    neckRotationOffset = Quaternion.Inverse(ConvertScneneKitSpaceToUnitySpace(new Vector3(0, 0, 0))) * neckTransform.rotation;
+                    startPosition = neckTransform.localPosition;
+                }
+            }
+
+            if (spineTransformEnabled)
+            {
+                if (spineTransform == null)
+                {
+                    isEveryThingConfigured = false;
+                    Debug.LogWarning("Face Cap Live Mode Receiver Error : Spine transform is not assigned.");
+                }
+                else
+                {
+                    spineRotationInitial = spineTransform.rotation;
+                    spineRotationOffset = Quaternion.Inverse(ConvertScneneKitSpaceToUnitySpace(new Vector3(0, 0, 0))) * spineTransform.rotation;
+                    startPosition = spineTransform.localPosition;
+                }
+            }
+        }
+
+        if (useEyeDirectionData)
+        {
+            if (leftEyeTransform == null && rightEyeTransform == null)
+            {
+                isEveryThingConfigured = false;
+                Debug.LogWarning("Face Cap Live Mode Receiver Error : Assign at least 1 eye transform.");
+            }
+
+            if (leftEyeTransform != null)
+            {
+                leftEyeRotationOffset = Quaternion.Inverse(ConvertScneneKitSpaceToUnitySpace(new Vector3(0, 0, 0))) * leftEyeTransform.rotation;
+            }
+
+            if (rightEyeTransform != null)
+            {
+                rightEyeRotationOffset = Quaternion.Inverse(ConvertScneneKitSpaceToUnitySpace(new Vector3(0, 0, 0))) * rightEyeTransform.rotation;
+            }
+        }
+
+        if (!isEveryThingConfigured)
+        {
+            return;
+        }
+
+        // Setup OSC Receiver
+
+        _OSCReceiver = gameObject.AddComponent<OSCReceiver>();
+
+        _OSCReceiver.LocalPort = _OSCReceiverPort;
+
+        if (usePositionData)
+        {
+            _OSCReceiver.Bind(_Position, PositionReceived);
+        }
+
+        if (useRotationData)
+        {
+            _OSCReceiver.Bind(_EulerAngles, EulerAnglesReceived);
+        }
+
+        if (useEyeDirectionData)
+        {
+            _OSCReceiver.Bind(_LeftEyeEulerAngles, LeftEyeEulerAnglesReceived);
+            _OSCReceiver.Bind(_RightEyeEulerAngles, RightEyeEulerAnglesReceived);
+        }
+
+        _OSCReceiver.Bind(_Blendshapes, BlendshapeReceived);
+
+    }
+
+    protected void PositionReceived(OSCMessage message)
+    {
+        Vector3 value;
+        if (message.ToVector3(out value) && usePositionData)
+        {
+            value.x *= -1;
+
+            if (spineTransformEnabled)
+            {
+                spineTransform.localPosition = startPosition + value; 
+            }
+            else if (neckTransformEnabled)
+            {
+                neckTransform.localPosition = startPosition + value;
+            }
+            else
+            {
+                headTransform.localPosition = startPosition + value;
+            }
+        }
+    }
+
+    protected void EulerAnglesReceived(OSCMessage message)
+    {
+        Vector3 value;
+        if (message.ToVector3(out value) && useRotationData)
+        {
+            Quaternion sceneKitRotation = ConvertScneneKitSpaceToUnitySpace(value);
+
+            if (spineTransformEnabled)
+            {
+                Quaternion newRotation = sceneKitRotation * spineRotationOffset;
+
+                spineTransform.rotation = Quaternion.Lerp(spineRotationInitial,newRotation, spineTransformBlendFactor);
+            }
+
+            if (neckTransformEnabled)
+            {
+                Quaternion newRotation = sceneKitRotation * neckRotationOffset;
+
+                neckTransform.rotation = Quaternion.Lerp(neckRotationInitial, newRotation, neckTransformBlendFactor);
+            }
+
+            headTransform.rotation =  ConvertScneneKitSpaceToUnitySpace(value) * headRotationOffset;
+        }
+    }
+
+    protected void LeftEyeEulerAnglesReceived(OSCMessage message)
+    {
+        Vector2 value;
+        if (message.ToVector2(out value) && leftEyeTransform != null)
+        {
+            Matrix4x4 inMatrix = Matrix4x4.Rotate(ConvertScneneKitSpaceToUnitySpace(new Vector3(value.x, value.y, 0)));
+            leftEyeTransform.rotation =  ConvertScneneKitSpaceToUnitySpace( new Vector3(value.x,value.y,0)) * leftEyeRotationOffset;
+        }
+    }
+
+    protected void RightEyeEulerAnglesReceived(OSCMessage message)
+    {
+        Vector2 value;
+        if (message.ToVector2(out value) && rightEyeTransform != null)
+        {
+            Matrix4x4 inMatrix = Matrix4x4.Rotate(ConvertScneneKitSpaceToUnitySpace(new Vector3(value.x, value.y, 0)));
+            rightEyeTransform.rotation =  ConvertScneneKitSpaceToUnitySpace(new Vector3(value.x, value.y, 0)) * rightEyeRotationOffset;
+        }
+    }
+
+    protected void BlendshapeReceived(OSCMessage message)
+    {
+        int index = 0;
+        float value = 0;
+
+        if (message.ToInt(out index) && message.ToFloat(out value))
+        {
+            for (int i = 0; i < blendShapeIndexes.Length; i++)
+            {
+                if (blendShapeIndexes[i] == index)
+                {
+                    smr.SetBlendShapeWeight(i, value * 100f);
+                }
+            }
+        }
+    }
+
+    protected Quaternion ConvertScneneKitSpaceToUnitySpace(Vector3 eulerAngles)
+    {
+        Quaternion q = Quaternion.Euler(eulerAngles);
+        return new Quaternion(q.x, q.y, -q.z, -q.w);
+    }
+
+}
